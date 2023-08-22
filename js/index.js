fc26a7af18866c49be11b3458ea144d93001092c
klhorne <123809708+klhorne@users.noreply.github.com>
2023-08-21 22:19:08 -0500

    8/21/23 1018pm

---
+++ js/index.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Your index.js file is loaded correctly!");
+
+    // Navigation scrolling
+    const sections = {
+        work: document.querySelector('.mywork1'),
+        about: document.querySelector('.aboutoverview'),
+        resume: document.querySelector('.resumecenterpage'),
+        contact: document.querySelector('.contact'),
+    };
+
+    const navItems = {
+        work: document.querySelector('.nav-work'),
+        about: document.querySelector('.nav-about'),
+        resume: document.querySelector('.nav-resume'),
+        contact: document.querySelector('.nav-contact'),
+    };
+
+    Object.keys(navItems).forEach(sectionKey => {
+        navItems[sectionKey].addEventListener('click', () => scrollToSection(sections[sectionKey]));
+    });
+
+    function scrollToSection(section) {
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+
+
